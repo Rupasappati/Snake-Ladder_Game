90848dc8020f93acfb5c782112267c6999fddbcf
Rupasappati <rupasappati@gmail.com>
2022-05-07 13:46:27 +0530

    Added: Check For a Player Options

--- SnakeAndLadderGame/Snake_Ladder.cs
+++ SnakeAndLadderGame/Snake_Ladder.cs
@@ -8,19 +8,42 @@ namespace SnakeAndLadderGame
 {
     public class Snake_Ladder
     {
-        int StartPosition = 0;
+        
         public const int NOPLAY = 0;
         public const int LADDER = 1;
         public const int SNAKE = 2;
-        int Dice;
-        int option;
+        int PlayerOptions;
         
         Random Dicenum = new Random();
         public void gamestart()
         {
-            int dice=Dicenum.Next(1,7);
+            int StartPosition = 0;
+            int Dice = 0;
+            int dice=Dicenum.Next(1, 7);
             Console.WriteLine( "Player Starts at a Position {0}", StartPosition);
+            Console.WriteLine(dice);
+
+            int Options=Dicenum.Next(1, 4);
+            Console.WriteLine("Number present on Dice {0}", Options);
+            switch (PlayerOptions)
+            {
+                case LADDER:
+
+                    StartPosition += Dice;
+                    Console.WriteLine("current position of a player is {0}", StartPosition);
+                    break;
+
+                case SNAKE:
+
+                    StartPosition -= Dice;
+                    Console.WriteLine("current position of a player is {0}", StartPosition);
+                    break;
+                                    
+            }
+            Console.WriteLine(StartPosition);
+
 
         }
+
     }
 }
