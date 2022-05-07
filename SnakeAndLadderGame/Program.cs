edecb9cfcd3e4199db0ea1fb47cb494b06422342
Rupasappati <rupasappati@gmail.com>
2022-05-07 12:37:15 +0530

    Start functionality added

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SnakeAndLadderGame
+{
+    class Programm
+    {
+        static void Main(String[] args)
+        {
+            Snake_Ladder game = new Snake_Ladder();
+            game.gamestart();
+        }
+    }
+}
+
---
+++ SnakeAndLadderGame/Snake_Ladder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    public class Snake_Ladder
+    {
+        int StartPosition = 0;
+        public const int NOPLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;    
+        
+        Random Dicenum = new Random();
+        public void gamestart()
+        {
+            int dice=Dicenum.Next(1,7);
+            Console.WriteLine( "Player Starts at a Position {0}", StartPosition);
+
+        }
+    }
+}
